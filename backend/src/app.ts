72b6c60d826640496dd1938ab4a8dbc6394daf5f
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2024-05-21 16:29:50 +0530

    Fixed issue for creating swagger api docs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,10 @@ declare global {
     }
   }
 }
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }'
+}))
 
 //Redirect /auth
 app.use("/auth", authRoute);
